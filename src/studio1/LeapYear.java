9f590e821df58678647ff46f655859873832a1b3
bari.b <bari.b@10.232.154.118>
2024-01-24 17:23:00 -0600

    we did great work :D! studio 1

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,17 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Year of choice?");
+		int year = in.nextInt();
+		boolean four = year/4;
+		boolean !hund = year/100;
+		
+	}
+
+}
--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,6 +15,14 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+boolean compxya = (x < y);
+boolean compyza = (z > y);
+boolean ascend = compxya && compyza; 
+boolean compxyd = (x > y);
+boolean compyzd = (z < y);
+boolean descend = compxyd && compyzd;
+boolean isOrdered = ascend || descend;
+System.out.print(isOrdered);
 
 	}
 
